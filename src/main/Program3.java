2bb4d3d9214016411bc2920ca5241c11ac71c2ed
ritz-bnsl <ritika.bansal_cs18@gla.ac.in>
2019-10-02 16:28:16 +0530

    init commit; create program3 class add sacnner; add input final commit;

---
+++ src/main/Program3.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Scanner;
+
+public class Program3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String name = s.next().trim();
+        int l = name.length();
+        name = name.toLowerCase();
+        char ch;
+        for (int i = 0; i < l; i++) {
+            ch = name.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                name = name.replace(ch, '*');
+            }
+        }
+        System.out.println(name);
+    }
+}
