39b49104c07d2e22b09530ac644d535f4b32d11e
ritz-bnsl <ritika.bansal_cs18@gla.ac.in>
2019-10-02 21:22:07 +0530

    init commit; create program9 class add sacnner; add input final commit;

---
+++ src/main/Program9.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.util.Scanner;
+
+public class Program9 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();
+        char ch1;
+        int l = 0, u = 0;
+        int abs = 0;
+        int ll = name.length();
+        for (int i = 0; i < ll; i++) {
+
+            ch1 = name.charAt(i);
+            if (ch1 >= 65 && ch1 <= 91) {
+                u += (int) ch1;
+            } else if (ch1 >= 97 && ch1 <= 122) {
+                l += (int) ch1;
+            }
+        }
+        abs = u - l;
+        System.out.println(Math.abs(abs));
+
+    }
+}
