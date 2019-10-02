95e9956d485079214e15088bfcfc1f29329eeac9
ritz-bnsl <ritika.bansal_cs18@gla.ac.in>
2019-10-02 20:14:06 +0530

    init commit; create program6 class add sacnner; add input final commit;

---
+++ src/main/Program6.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Scanner;
+
+public class Program6 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();
+        char ch = s.nextLine().charAt(0);
+        int l = name.length();
+        System.out.println("output:");
+        for (int i = 0; i < l; i++) {
+            char ch1 = name.charAt(i);
+            if (ch1 == ch) {
+                System.out.println(i);
+
+            }
+        }
+    }
+}
